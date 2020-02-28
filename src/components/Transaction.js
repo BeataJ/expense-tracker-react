da2fb077e06cfe813853ec20dc97be6e8d81e111
beata <beata@emc2wave.com>
2020-02-28 13:58:31 -0800

    add GlobalContext addTransaction

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -3,6 +3,7 @@ import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({ transaction }) => {
   const { deleteTransaction } = useContext(GlobalContext);
+
   const sign = transaction.amount < 0 ? '-' : '+';
 
   return (
